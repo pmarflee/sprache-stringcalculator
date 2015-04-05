e27c66890a715118bec579cecb36efb5133190f8
Paul Marfleet <paul.marfleet@gmail.com>
2015-04-05 17:19:47 +0100

    Initial commit.  Tests passing for empty input.

---
+++ StringCalculatorCSharp/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorCSharp
+{
+    class StringCalculator
+    {
+        public int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ StringCalculatorCSharp/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace StringCalculatorCSharp
+{
+    [TestFixture]
+    public class Tests
+    {
+        private StringCalculator _stringCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void ShouldReturnZeroForAnEmptyString()
+        {
+            Assert.AreEqual(0, _stringCalculator.Add(String.Empty));
+        }
+    }
+}
